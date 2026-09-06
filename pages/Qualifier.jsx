0e76b9471bac8f2dc24649a9eda4b019cfdbdf4d
JANA <janaelsaeed20@gmail.com>
2026-09-06 15:25:02 -0400

    ok

--- pages/Qualifier.jsx
+++ pages/Qualifier.jsx
@@ -164,7 +164,7 @@ export default function Qualifier() {
                         <span>ALLOCATION DATE</span>
 
                         <strong>
-                            14 SEP
+                            30 SEP
                             <br />
                             2026
                         </strong>
